88486cb928c75a49f6be28aaeaafa5e5dbe12e79
bilalozair <bo569@nyu.edu>
2022-03-24 01:17:44 -0400

    commit tree f70168b44370065711ffe15837df62d190480dc5

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "1975", result: "N/A"},
+    { year: "1974", result: "N/A"},
+    { year: "1973", result: "N/A"},
+    { year: "1972", result: "N/A"},
+    { year: "1971", result: "N/A"},
+    { year: "1970", result: "N/A"},
+    { year: "1969", result: "N/A"},
+    { year: "1968", result: "N/A"},
+    { year: "1967", result: "N/A"},
+    { year: "1966", result: "N/A"},
+    { year: "1965", result: "N/A"},
+    { year: "1964", result: "N/A"},
+    { year: "1963", result: "N/A"},
+    { year: "1962", result: "N/A"},
+    { year: "1961", result: "N/A"},
+    { year: "1960", result: "N/A"}
+  ];
+
+function superbowlWin(stats) {
+   let foundRecord = (stats.find(element => element.result === "W"));
+   if (foundRecord != undefined) {
+    return foundRecord.year;
+   } else {
+    return undefined;
+   }
+}
+superbowlWin(record);
